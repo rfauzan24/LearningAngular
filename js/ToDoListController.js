86821532aa5fdc38192214b52173e9afd351c32a
Rizky Fauzan Bahtiar <rizky.fauzan2408@gmail.com>
2018-07-27 11:29:27 +0700

    Update indentation controller

--- js/ToDoListController.js
+++ js/ToDoListController.js
@@ -1,66 +1,71 @@
 'use strict';
-        angular.module('TodoListManager', [])
+    angular.module('TodoListManager', [])
         .controller('ToDoListController', ToDoListController)
         .controller('CompletedItemsController', CompletedItemsController)
         .service('ToDoListService', ToDoListService);
         ToDoListController.$inject = ['ToDoListService'];
         CompletedItemsController.$inject = ['ToDoListService'];
         ToDoListService.$inject = ['$filter'];
-        
+
         function ToDoListController(ToDoListService) {
             var showList = this;
-                showList.items = ToDoListService.getTodoList();
-                showList.newItem = "";
-                showList.completeToDoItem = function(itemIndex){
-            var itemName = showList.items[itemIndex]["name"];
-            ToDoListService.addCompletedItem(itemName);
-            ToDoListService.removeToDoItem(itemIndex);
-                };
-                
-                showList.deleteToDoItem = function(itemIndex){
+            showList.items = ToDoListService.getTodoList();
+            showList.newItem = "";
+            showList.completeToDoItem = function(itemIndex) {
+                var itemName = showList.items[itemIndex]["name"];
+                ToDoListService.addCompletedItem(itemName);
                 ToDoListService.removeToDoItem(itemIndex);
-                };
+            };
                 
-                showList.addNewItem = function () {
+            showList.deleteToDoItem = function(itemIndex){
+                ToDoListService.removeToDoItem(itemIndex);
+            };
+                
+            showList.addNewItem = function () {
                 ToDoListService.addToDoItem(showList.newItem);
-                }
+            }
         }
+
         function CompletedItemsController(ToDoListService) {
             var showList = this;
             showList.items = ToDoListService.getCompletedList();
             showList.deleteCompletedItem = function(itemIndex){
-            ToDoListService.removeCompletedItem(itemIndex);
+                ToDoListService.removeCompletedItem(itemIndex);
             };
         }
+
         function ToDoListService($filter) {
             var service = this;
             var todoList = [];
             var completedList = [];
             service.getTodoList = function () {
-            return todoList;
+                return todoList;
             };
             
             service.getCompletedList = function () {
-            return completedList;
+                return completedList;
             };
+
             service.removeToDoItem = function (itemIndex) {
-            todoList.splice(itemIndex, 1);
+                todoList.splice(itemIndex, 1);
             };
+
             service.removeCompletedItem = function(itemIndex){
                 completedList.splice(itemIndex,1);
             };
             
             service.addToDoItem = function(itemName){
-            var item = {
-                name: itemName
+                var item = {
+                    name: itemName
                 };
                 todoList.push(item);
             };
+
             service.addCompletedItem = function(itemName){
-            var finalStr = itemName;;
-            var item = {
-                name: finalStr
+                var finalStr = itemName;;
+                var item = {
+                    name: finalStr
                 };
                 completedList.push(item);
             };
-    }
+        }
